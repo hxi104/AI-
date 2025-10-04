d71a3c9751991971d4e28e79bdadf56ad39b7db2
Harry <harryirving32@gmail.com>
2025-10-04 02:49:07 +0100

    continue part 2

--- tiles.py
+++ tiles.py
@@ -7,7 +7,7 @@ Created on Fri Oct  3 23:08:01 2025
 
 class State:
     
-    def __init__(self, grid, pos = None):
+    def __init__(self, grid, pos= None):
         
         """
         grid: list of lists (n x n)
@@ -18,7 +18,7 @@ class State:
         self.size = len(grid)
         
         #copy grid
-        self.grid = [[grid[i][j] for j in range(self.size)] for i in range(self.size)]
+        self.grid = grid
         
         if pos is None:
             for i in range(self.size):
@@ -37,15 +37,72 @@ class State:
         for row in self.grid:
             rows.append(' '.join(f"{v:2d}" if v !=0 else " _" for v in row))
         return '\n'.join(rows)
+    
+    def clone(self):
+        cPos = self.pos
+        cGrid = [[self.grid[i][j] for j in range(self.size)] for i in range(self.size)]
+        return State(cGrid,cPos)
         
-        
+    def moveUp(self):
+        i,j = self.pos
+        if i > 0: #check empty not in top row
+            st = self.clone() #clone state
+            st.grid[i][j],st.grid[i-1][j] = st.grid[i-1][j],st.grid[i][j] #swap empty with above tile
+            st.pos = (i-1,j) #update position
+            return st
+        return None #move not possible
+    
+    def moveDown(self):
+        i,j = self.pos
+        if i+1 < self.size:
+            st = self.clone()
+            st.grid[i][j],st.grid[i+1][j] = st.grid[i+1][j],st.grid[i][j]
+            st.pos = (i+1,j)
+            return st
+        return None
+    
+    def moveLeft(self):
+        i,j = self.pos
+        if j > 0:
+            st = self.clone()
+            st.grid[i][j],st.grid[i][j-1] = st.grid[i][j-1],st.grid[i][j]
+            st.pos = (i,j-1)
+            return st
+        return None
+    
+    def moveRight(self):
+        i,j = self.pos
+        if j+1 < self.size:
+            st = self.clone()
+            st.grid[i][j],st.grid[i][j+1] = st.grid[i][j+1],st.grid[i][j]
+            st.pos = (i,j+1)
+            return st
+        return None
+    
+    def move(self):
+        actions = [self.moveDown,self.moveUp,self.moveRight,self.moveLeft]
+        for fn in actions:
+            result = fn()
+            if result is not None:
+                yield result
+                
+
     
 def tester():
-    grid = [[1,0,3],[4,2,5],[6,7,8]]
+    grid = [[1,2,3],[4,0,5],[6,7,8]]
     st = State(grid)
-    print("empty tile position:", st.pos)
     
-    print(st)
+    
+    for next_state in st.move():
+        print(next_state)
+        
+    print("second test")
+    
+    grid2 = [[1,2,3],[4,5,0],[6,7,8]]
+    sa = State(grid2)
+    for next_state in sa.move():
+        print(next_state)
+    
 
 
 if __name__ == "__main__":
